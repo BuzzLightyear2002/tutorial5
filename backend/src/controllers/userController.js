290a4a24e38a23711a644f9be8331c010cffbf80
Raj Chauhan <rajbhavesh2002@gmail.com>
2024-07-11 18:46:31 -0300

    Last Change

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,6 @@
 const userService = require('../services/userService');
 
+// GET all users
 exports.getUsers = async (req, res, next) => {
   try {
     const users = await userService.getAllUsers();
@@ -9,15 +10,20 @@ exports.getUsers = async (req, res, next) => {
   }
 };
 
+// GET a specific user by ID
 exports.getUser = async (req, res, next) => {
   try {
     const user = await userService.getUserById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
     res.json(user);
   } catch (error) {
     next(error);
   }
 };
 
+// POST route to add a new user
 exports.createUser = async (req, res, next) => {
   try {
     const user = await userService.createUser(req.body);
@@ -27,9 +33,13 @@ exports.createUser = async (req, res, next) => {
   }
 };
 
+// PUT route to update a user by ID
 exports.updateUser = async (req, res, next) => {
   try {
     const user = await userService.updateUser(req.params.id, req.body);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
     res.json(user);
   } catch (error) {
     next(error);
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/', userController.getUsers);
-router.get('/:id', userController.getUser);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
+// GET all users
+router.get('/users', userController.getUsers);
+
+// GET a specific user by ID
+router.get('/user/:id', userController.getUser);
+
+// POST route to add a new user
+router.post('/add', userController.createUser);
+
+// PUT route to update a user by ID
+router.put('/update/:id', userController.updateUser);
 
 module.exports = router;
