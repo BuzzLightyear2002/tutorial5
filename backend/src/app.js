718fc94b9c559d4b59c97d92962c43b07b14ad3f
Raj Chauhan <rajbhavesh2002@gmail.com>
2024-07-11 18:35:52 -0300

    Changing THE API PATH according the API path in the doc

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
-app.use('/api/users', userRoutes);
+app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send('Not Found');
